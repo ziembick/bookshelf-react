1cab6e15b0d717760a62288239288a464646ff18
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-04 21:02:04 -0300

    books js

---
+++ bookshelf/src/Book.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Book ({ book, shelf, onMoveBook}) {
+    return (
+        <li>
+            <div>
+                
+            </div>
+        </li>
+    )
+}
