a6bda9842b33a6f03c5375334f21689bdab729a5
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-06 20:40:58 -0300

    bookshelf book map

--- bookshelf/src/BookShelf.js
+++ bookshelf/src/BookShelf.js
@@ -1,26 +1,24 @@
 import React from "react";
 import Book from "./Book";
 
-function Bookshelf ({ books, onMoveBook, shelfTitle }) {
-    return (
-        <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelfTitle}</h2>
-                <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {books
-                      .filter((book) => book.shelf === "currentlyReading")
-                      .map((book) => (
-                        <Book
-                          key={book.id}
-                          book={book}
-                          shelf={book.shelf}
-                          onMoveBook={onMoveBook}
-                        />
-                      ))}
-                  </ol>
-                </div>
-              </div>
-    )
+function Bookshelf({ books, onMoveBook, shelfTitle }) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{shelfTitle}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {books.map((book) => (
+            <Book
+              key={book.id}
+              book={book}
+              shelf={book.shelf}
+              onMoveBook={onMoveBook}
+            />
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
 }
 
-export default Bookshelf
+export default Bookshelf;
