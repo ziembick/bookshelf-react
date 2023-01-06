12b9ad05c561224b2bdbe61d44cd17567ed53e0f
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-06 20:40:04 -0300

    bookshelf return content

--- bookshelf/src/BookShelf.js
+++ bookshelf/src/BookShelf.js
@@ -3,7 +3,23 @@ import Book from "./Book";
 
 function Bookshelf ({ books, onMoveBook, shelfTitle }) {
     return (
-
+        <div className="bookshelf">
+                <h2 className="bookshelf-title">{shelfTitle}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {books
+                      .filter((book) => book.shelf === "currentlyReading")
+                      .map((book) => (
+                        <Book
+                          key={book.id}
+                          book={book}
+                          shelf={book.shelf}
+                          onMoveBook={onMoveBook}
+                        />
+                      ))}
+                  </ol>
+                </div>
+              </div>
     )
 }
 
