d8d48e801e0e1d24dc81de1ce19a6c8387ed69f2
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-08 19:50:03 -0300

    search books function

--- bookshelf/src/SearchBooks.js
+++ bookshelf/src/SearchBooks.js
@@ -1,3 +1,19 @@
 import React from "react";
 import { useEffect } from "react";
 import Book from "./Book";
+
+function SearchBooks({ query, setQuery, books, moveBook, setShowSearchpage }) {
+  useEffect(() => {
+    setQuery("");
+  }, [setQuery]);
+
+  const handleCloseSearch = () => {
+    setShowSearchpage(false);
+  };
+
+  return (
+    
+  );
+}
+
+export default SearchBooks;
