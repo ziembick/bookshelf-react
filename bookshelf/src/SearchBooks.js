1a609542e542dc663c1f4969181a49036ba74b81
Paulo Ziembick <pauloziembick@gmail.com>
2023-01-08 19:50:29 -0300

    search books return

--- bookshelf/src/SearchBooks.js
+++ bookshelf/src/SearchBooks.js
@@ -12,7 +12,39 @@ function SearchBooks({ query, setQuery, books, moveBook, setShowSearchpage }) {
   };
 
   return (
-    
+    <div className="search-books">
+      <div className="search-books-bar">
+        <button className="close-search" onClick={handleCloseSearch}>
+          Close
+        </button>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+        </div>
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {books
+            .filter(
+              (book) =>
+                book.title.toLowerCase().includes(query) ||
+                book.authors.join(", ").toLowerCase().includes(query)
+            )
+            .map((book) => (
+              <Book
+                key={book.id}
+                book={book}
+                shelf={book.shelf}
+                onMoveBook={moveBook}
+              />
+            ))}
+        </ol>
+      </div>
+    </div>
   );
 }
 
